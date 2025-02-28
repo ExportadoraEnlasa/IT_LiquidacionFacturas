d49c0825d764772e344b7df93f43a8dd0df25975
JeffersonIB <99jeffersonk9986@gmail.com>
2025-02-28 11:16:15 -0600

    Cambios 2

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -13,19 +13,23 @@ namespace IT_LiquidacionFacturas
                 "~/vendors/bootstrap/dist/css/bootstrap.min.css"
             ));
             bundles.Add(new ScriptBundle("~/Vendors/bootstrap/JS").Include(
-                "~/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"
+                "~/vendors/bootstrap/dist/js/bootstrap.bundle.min.js" 
+
             ));
             bundles.Add(new StyleBundle("~/Vendors/font-awesome").Include(
                 "~/vendors/font-awesome/css/font-awesome.min.css"
+
             ));
             bundles.Add(new StyleBundle("~/Vendors/nprogress/CSS").Include(
                 "~/vendors/nprogress/nprogress.css"
+
             ));
             bundles.Add(new ScriptBundle("~/Vendors/nprogress/JS").Include(
                 "~/vendors/nprogress/nprogress.js"
             ));
             bundles.Add(new StyleBundle("~/Vendors/iCheck/CSS").Include(
                 "~/vendors/iCheck/skins/flat/green.css"
+
             ));
             bundles.Add(new ScriptBundle("~/Vendors/iCheck/JS").Include(
                 "~/vendors/iCheck/icheck.min.js"
