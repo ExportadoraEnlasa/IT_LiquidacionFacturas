defc76a625274ade815d083003ac3e18e9b6a55c
JeffersonIB <99jeffersonk9986@gmail.com>
2025-02-28 11:15:04 -0600

    Cambios
    Cambios

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,23 +9,31 @@ namespace IT_LiquidacionFacturas
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Vendors/bootstrap").Include(
-                "~/vendors/bootstrap/dist/css/bootstrap.min.css",
+            bundles.Add(new StyleBundle("~/Vendors/bootstrap/CSS").Include(
+                "~/vendors/bootstrap/dist/css/bootstrap.min.css"
+            ));
+            bundles.Add(new ScriptBundle("~/Vendors/bootstrap/JS").Include(
                 "~/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"
             ));
             bundles.Add(new StyleBundle("~/Vendors/font-awesome").Include(
                 "~/vendors/font-awesome/css/font-awesome.min.css"
             ));
-            bundles.Add(new StyleBundle("~/Vendors/nprogress").Include(
-                "~/vendors/nprogress/nprogress.css",
+            bundles.Add(new StyleBundle("~/Vendors/nprogress/CSS").Include(
+                "~/vendors/nprogress/nprogress.css"
+            ));
+            bundles.Add(new ScriptBundle("~/Vendors/nprogress/JS").Include(
                 "~/vendors/nprogress/nprogress.js"
             ));
-            bundles.Add(new StyleBundle("~/Vendors/iCheck").Include(
-                "~/vendors/iCheck/skins/flat/green.css",
+            bundles.Add(new StyleBundle("~/Vendors/iCheck/CSS").Include(
+                "~/vendors/iCheck/skins/flat/green.css"
+            ));
+            bundles.Add(new ScriptBundle("~/Vendors/iCheck/JS").Include(
                 "~/vendors/iCheck/icheck.min.js"
             ));
-            bundles.Add(new StyleBundle("~/Vendors/bootstrap-progressbar").Include(
-                "~/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css",
+            bundles.Add(new StyleBundle("~/Vendors/bootstrap-progressbar/CSS").Include(
+                "~/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css"
+            ));
+            bundles.Add(new ScriptBundle("~/Vendors/bootstrap-progressbar/JS").Include(
                 "~/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"
             ));
             bundles.Add(new StyleBundle("~/Vendors/jqvmap").Include(
@@ -37,55 +45,52 @@ namespace IT_LiquidacionFacturas
             bundles.Add(new StyleBundle("~/build/css").Include(
                "~/build/css/custom.min.css"
            ));
-            bundles.Add(new StyleBundle("~/vendors/jquery").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/jquery").Include(
                "~/vendors/jquery/dist/jquery.min.js"
            ));
-            bundles.Add(new StyleBundle("~/vendors/fastclick").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/fastclick").Include(
                "~/vendors/fastclick/lib/fastclick.js"
            ));
-            bundles.Add(new StyleBundle("~/vendors/Chart.js").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/Chart.js").Include(
                "~/vendors/Chart.js/dist/Chart.min.js"
            ));
-            bundles.Add(new StyleBundle("~/vendors/gauge.js").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/gauge.js").Include(
                "~/vendors/gauge.js/dist/gauge.min.js"
            ));
-            bundles.Add(new StyleBundle("~/vendors/skycons").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/skycons").Include(
                "~/vendors/skycons/skycons.js"
            ));
-            bundles.Add(new StyleBundle("~/vendors/Flot").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/Flot").Include(
               "~/vendors/Flot/jquery.flot.js",
               "~/vendors/Flot/jquery.flot.pie.js",
               "~/vendors/Flot/jquery.flot.time.js",
               "~/vendors/Flot/jquery.flot.stack.js",
               "~/vendors/Flot/jquery.flot.resize.js"
           ));
-            bundles.Add(new StyleBundle("~/vendors/flot.orderbars").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/flot.orderbars").Include(
                "~/vendors/flot.orderbars/js/jquery.flot.orderBars.js"
            ));
-            bundles.Add(new StyleBundle("~/vendors/flot-spline").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/flot-spline").Include(
                "~/vendors/flot-spline/js/jquery.flot.spline.min.js"
            ));
-            bundles.Add(new StyleBundle("~/vendors/flot.curvedlines").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/flot.curvedlines").Include(
               "~/vendors/flot.curvedlines/curvedLines.js"
           ));
-            bundles.Add(new StyleBundle("~/vendors/DateJS").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/DateJS").Include(
               "~/vendors/DateJS/build/date.js"
           ));
-            bundles.Add(new StyleBundle("~/vendors/jqvmap").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/jqvmap").Include(
               "~/vendors/jqvmap/dist/jquery.vmap.js",
               "~/vendors/jqvmap/dist/maps/jquery.vmap.world.js",
               "~/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"
           ));
-            bundles.Add(new StyleBundle("~/vendors/moment").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/moment").Include(
              "~/vendors/moment/min/moment.min.js"
           ));
-            bundles.Add(new StyleBundle("~/vendors/moment").Include(
-             "~/vendors/moment/min/moment.min.js"
-          ));
-            bundles.Add(new StyleBundle("~/vendors/bootstrap-daterangepicker").Include(
+            bundles.Add(new ScriptBundle("~/Vendors/bootstrap-daterangepicker").Include(
              "~/vendors/bootstrap-daterangepicker/daterangepicker.js"
           ));
-            bundles.Add(new StyleBundle("~/build/js").Include(
+            bundles.Add(new ScriptBundle("~/build/js").Include(
              "~/build/js/custom.min.js"
           ));
         }
