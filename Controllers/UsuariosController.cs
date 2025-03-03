4691184e8df73ec0fa89954f5212db2f26f25035
JeffersonIB <99jeffersonk9986@gmail.com>
2025-03-03 11:58:38 -0600

    Actualización Usuarios

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_LiquidacionFacturas.Controllers
+{
+    public class UsuariossController : Controller
+    {
+        // GET: Usuarioss
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿// Models/Usuarios.cs
+using System;
+
+namespace IT_LiquidacionFacturas.Models
+{
+    public class Usuarios
+    {
+        public long IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string UsuarioNombre { get; set; } // Cambiado para evitar confusión con la propiedad 'Usuario'
+        public byte[] Contrasena { get; set; }
+        public int IdRol { get; set; }
+        public DateTime FechaCrea { get; set; }
+        public DateTime FechaModifica { get; set; }
+        public bool Estado { get; set; }
+    }
+}
